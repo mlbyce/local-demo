07bbcfe81f530610fc324cb059c4864bd751af8b
Mike Byce <mike@metageek.net>
2020-01-09 11:06:43 -0700

    Working

--- src/writeToTable/index.js
+++ src/writeToTable/index.js
@@ -1,6 +1,41 @@
-exports.handler = async (event, context) => {
-  // Log the event argument for debugging and for use in local development.
-  console.log(JSON.stringify(event, undefined, 2));
+const AWS = require('aws-sdk');
 
-  return {};
+exports.handler = async (event, context) => {
+  const db = new AWS.DynamoDB.DocumentClient();
+  let promiseArray = [];
+
+  for (let i = 0; i < 10; i++) {
+    const params = {
+      TableName: process.env.TABLE_NAME,
+      Item: {
+        id: `X${i}X`,
+        title: `Item #${i}#`,
+        content: 'Some Bogus Content',
+        funk: 'Funk Data'
+      },
+      ConditionExpression: 'attribute_not_exists(id)',
+      ReturnConsumedCapacity: 'TOTAL'
+    };
+    const p = db.put(params)
+      .promise()
+      .then(() => {
+        console.log(`Writing item ${i} to the DB`);
+      })
+      .catch((error) => {
+        console.log('Some Badness');
+        console.log(error.message);
+      });
+
+    promiseArray.push(p);
+  }
+
+  try {
+    await Promise.all(promiseArray);
+    console.log('Real Success');
+  }
+  catch (error) {
+    console.log(err);
+  }
+
+  return "Success";
 };
